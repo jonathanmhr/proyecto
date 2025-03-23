deee63d0acf6eb003cf783893d5c4b651c292d5b
jonathan.michieli <jonathanmichelirosas@gmail.com>
2025-03-23 20:19:01 +0100

    cambios en el correo de personalizacion

--- resources/lang/es/auth.php
+++ resources/lang/es/auth.php
@@ -1,11 +1,11 @@
 <?php
 
 return [
-    'verify_email_subject' => 'Verifica tu dirección de correo electrónico',
-    'greeting' => 'Hola :name,',
+    'verify_email_subject' => 'Verifica tu correo electrónico',
+    'greeting' => 'Hola :name!',
     'line1' => 'Haz clic en el botón de abajo para verificar tu dirección de correo electrónico.',
     'action' => 'Verificar correo electrónico',
     'line2' => 'Si no creaste esta cuenta, no es necesario que hagas nada.',
-    'salutation' => 'Saludos, :app_name',  // Añadido aquí
+    'salutation' => 'Saludos, Power Core',
 ];
 
