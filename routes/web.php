02055fccf1fe559837e1aae4a2aff511c3396805
jonathan.michieli <jonathanmichelirosas@gmail.com>
2025-03-23 13:29:35 +0100

    cambios en web.php

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use Laravel\Fortify\Http\Controllers\VerifyEmailController;
+use Illuminate\Auth\Notifications\VerifyEmail;
 
 Route::get('/', function () {
     return view('welcome');
