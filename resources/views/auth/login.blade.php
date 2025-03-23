887de8a4099a6b7dd216e7bbef1eb6177ba49e63
jonathan.michieli <jonathanmichelirosas@gmail.com>
2025-03-23 11:55:26 +0100

    cambio de posicion para el login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -37,17 +37,17 @@
                     <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('password.request') }}">
                         {{ __('Olvidaste tu contraseña?') }}
                     </a>
+                    
+                    <!-- Botón para redirigir al formulario de registro -->
+                    <a href="{{ route('register') }}" class="ml-4 text-sm text-gray-600 hover:text-gray-900">
+                        {{ __('No tienes cuenta? Regístrate') }}
+                    </a>
                 @endif
 
                 <div class="flex items-center">
                     <x-button class="ms-4">
                         {{ __('Log in') }}
                     </x-button>
-
-                    <!-- Botón para redirigir al formulario de registro -->
-                    <a href="{{ route('register') }}" class="ml-4 text-sm text-gray-600 hover:text-gray-900">
-                        {{ __('No tienes cuenta? Regístrate') }}
-                    </a>
                 </div>
             </div>
         </form>
