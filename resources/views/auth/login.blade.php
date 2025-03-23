687feafe3474dcdc6a3a69e1d1bdfed223a8fcec
jonathan.michieli <jonathanmichelirosas@gmail.com>
2025-03-23 11:53:50 +0100

    boton para registrarse dentro del login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -32,16 +32,23 @@
                 </label>
             </div>
 
-            <div class="flex items-center justify-end mt-4">
+            <div class="flex items-center justify-between mt-4">
                 @if (Route::has('password.request'))
                     <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('password.request') }}">
                         {{ __('Olvidaste tu contraseña?') }}
                     </a>
                 @endif
 
-                <x-button class="ms-4">
-                    {{ __('Log in') }}
-                </x-button>
+                <div class="flex items-center">
+                    <x-button class="ms-4">
+                        {{ __('Log in') }}
+                    </x-button>
+
+                    <!-- Botón para redirigir al formulario de registro -->
+                    <a href="{{ route('register') }}" class="ml-4 text-sm text-gray-600 hover:text-gray-900">
+                        {{ __('No tienes cuenta? Regístrate') }}
+                    </a>
+                </div>
             </div>
         </form>
     </x-authentication-card>
