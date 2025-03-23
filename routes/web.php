6c61f5383ec958cec8809a2fe9d1f5b9b543885b
jonathan.michieli <jonathanmichelirosas@gmail.com>
2025-03-23 12:29:22 +0100

    cambios del web.php que no habia un controlador

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use Laravel\Fortify\Http\Controllers\VerificationController;
+use Laravel\Fortify\Http\Controllers\VerifyEmailController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -21,12 +21,7 @@ Route::get('/email/verify', function () {
     return view('auth.verify');
 })->middleware('auth')->name('verification.notice');
 
-// Ruta para verificar el correo electrónico
-Route::get('/email/verify/{id}/{hash}', [VerificationController::class, 'verify'])
-    ->middleware(['auth', 'signed'])
-    ->name('verification.verify');
-
-// Ruta para reenviar el correo de verificación
-Route::post('/email/verification-notification', [VerificationController::class, 'sendVerificationEmail'])
+// Usar VerifyEmailController en lugar de VerificationController
+Route::post('/email/verification-notification', [VerifyEmailController::class, 'sendVerificationEmail'])
     ->middleware(['auth', 'throttle:6,1'])
     ->name('verification.send');
