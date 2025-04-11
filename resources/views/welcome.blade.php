60ac42d1966c66770841b62abb376fa3881c452d
jonathan.michieli <jonathanmichelirosas@gmail.com>
2025-04-11 22:53:01 +0200

    estilos de ruta

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,8 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Bienvenido al Gimnasio</title>
-    <link rel="stylesheet" href="{{ vite('resources/css/app.css') }}">
-<script type="module" src="{{ vite('resources/js/app.js') }}"></script>
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 <body class="bg-gray-100 font-sans">
 
