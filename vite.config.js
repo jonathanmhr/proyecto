fce9aaff554a3225929c241ac243f1d1b1956a4d
jonathan.michieli <jonathanmichelirosas@gmail.com>
2025-04-12 21:41:08 +0200

    cambios para que funcione el vite.js

--- vite.config.js
+++ vite.config.js
@@ -2,16 +2,18 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
+    server: {
+        host: '0.0.0.0',
+        port: 5173,
+        strictPort: true,
+        hmr: {
+            host: '192.168.1.34',
+        },
+    },
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
         }),
     ],
-    server: {
-        port: 5173, // Asegúrate de que el puerto es el correcto
-        proxy: {
-            '/': 'http://localhost', // Redirige todas las solicitudes a Laravel
-        },
-    },
 });
