5c16bd219b9430decf0ff150705e7cb532554983
jonathan.michieli <jonathanmichelirosas@gmail.com>
2025-04-12 21:19:10 +0200

    se sube config del vite.js

--- vite.config.js
+++ vite.config.js
@@ -8,4 +8,9 @@ export default defineConfig({
             refresh: true,
         }),
     ],
+    server: {
+        proxy: {
+            '/': 'http://localhost', // Proxy para redirigir a Laravel
+        },
+    },
 });
