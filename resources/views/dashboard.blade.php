359099c394049d3d52742b887fcbe16f564b6385
jonathan.michieli <jonathanmichelirosas@gmail.com>
2025-04-12 18:14:19 +0000

    se actualiza todo

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -8,18 +8,18 @@
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg p-6">
                 @if ($perfil) <!-- Verifica si el perfil existe -->
-                    <p>Fecha de nacimiento: {{ $perfil->fecha_nacimiento }}</p>
-                    <p>Peso: {{ $perfil->peso }} kg</p>
-                    <p>Altura: {{ $perfil->altura }} cm</p>
-                    <p>Objetivo: {{ $perfil->objetivo }}</p>
+                <p>Fecha de nacimiento: {{ $perfil->fecha_nacimiento }}</p>
+                <p>Peso: {{ $perfil->peso }} kg</p>
+                <p>Altura: {{ $perfil->altura }} cm</p>
+                <p>Objetivo: {{ $perfil->objetivo }}</p>
                 @else
-                    <div class="mb-6">
-                        <p>¡No tienes un perfil completo! <a href="{{ route('perfil.edit') }}" class="text-blue-500">Haz clic aquí para completarlo.</a></p>
-                    </div>
-                @endif
-            </div>
+                <div class="flex justify-center items-center p-6 rounded-md shadow-lg mb-6">
+                    <p class="text-yellow-800 font-semibold">
+                        ¡No tienes un perfil completo!
+                        <a href="{{ route('perfil.edit') }}" class="text-blue-500 font-bold hover:underline">Haz clic aquí para completarlo.</a>
+                    </p>
+                </div>
         </div>
     </div>
 </x-app-layout>
