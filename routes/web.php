c87aa1b097dbc8e28376ba25741f06a0a9d8d3e9
jonathan.michieli <jonathanmichelirosas@gmail.com>
2025-04-12 18:54:22 +0200

    cambios para ver si se puede acceder al dashboard

--- routes/web.php
+++ routes/web.php
@@ -8,14 +8,11 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-// Ruta para el Dashboard (ya autenticadas y con verificación de email)
-Route::middleware(['auth', 'verified'])->get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
+// Ruta para el Dashboard (con autenticación y verificación de correo)
+Route::middleware(['auth', 'verified'])->get('/dashboard', function () {
+    return view('dashboard');
+})->name('dashboard');
 
-// Ruta para crear el perfil (Formulario de creación)
-Route::middleware('auth')->get('/perfil/create', [ProfileController::class, 'create'])->name('perfil.create');
-
-// Ruta para guardar el perfil (Acción de almacenar en base de datos)
-Route::middleware('auth')->post('/perfil', [ProfileController::class, 'store'])->name('perfil.store');
-
-// Ruta para mostrar el perfil (Para ver el perfil ya creado)
-Route::middleware('auth')->get('/perfil', [ProfileController::class, 'show'])->name('perfil.show');
+// Añadir aquí las rutas para el perfil
+Route::middleware(['auth', 'verified'])->get('/perfil/create', [ProfileController::class, 'create'])->name('perfil.create');
+Route::middleware(['auth', 'verified'])->post('/perfil/store', [ProfileController::class, 'store'])->name('perfil.store');
