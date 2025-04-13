5eb973168e7664e5cf424ad244f00841254325df
jonathan.michieli <jonathanmichelirosas@gmail.com>
2025-04-13 15:44:19 +0200

    cambioos para ver la imagen en dd

--- resources/views/profile/update-profile-information-form.blade.php
+++ resources/views/profile/update-profile-information-form.blade.php
@@ -28,6 +28,7 @@
 
             <!-- Current Profile Photo -->
             <div class="mt-2" x-show="! photoPreview">
+            {{ dd($this->user->profile_photo_url) }}
             <img src="{{ $this->user->profile_photo_url }}" alt="{{ $this->user->name }}" class="rounded-full size-20 object-cover">
             </div>
 
