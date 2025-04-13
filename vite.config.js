474e83d999d7ca887c1bcd2805ef8dd2bcf8ccd6
jonathan.michieli <jonathanmichelirosas@gmail.com>
2025-04-13 15:34:41 +0200

    cambios en vite config para el js

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,15 @@ import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
     server: {
-        host: '0.0.0.0',
+        host: '0.0.0.0', // Escucha en todas las interfaces
         port: 5173,
         strictPort: true,
+        origin: 'http://192.168.1.34:5173', // <- IMPORTANTE para que use la IP correcta
+        cors: true, // <- PERMITIR CORS
         hmr: {
-            host: '192.168.1.34',
             protocol: 'ws',
+            host: '192.168.1.34',
+            port: 5173,
         },
     },
     plugins: [
