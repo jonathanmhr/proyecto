77f4c70e130ee555a9bcc25af7f175bb4d0ebaff
jonathan.michieli <jonathanmichelirosas@gmail.com>
2025-04-13 14:52:12 +0200

    resturacion de imagenes

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -150,7 +150,7 @@
             <div class="flex items-center px-4">
                 @if (Laravel\Jetstream\Jetstream::managesProfilePhotos())
                     <div class="shrink-0 me-3">
-                    <img class="size-8 rounded-full object-cover" src="{{ Auth::user()->profile_photo_url }}" alt="{{ Auth::user()->name }}" />
+                    <img src="{{ Storage::url($this->user->profile_photo_path) }}" alt="{{ $this->user->name }}" class="rounded-full size-20 object-cover">
                     </div>
                 @endif
 
