4703fab26627b9e0e8dcd19e25a645aafc8e1442
jonathan.michieli <jonathanmichelirosas@gmail.com>
2025-04-25 23:43:34 +0200

    se crea nueva vista

--- resources/views/clases/index.blade.php
+++ resources/views/clases/index.blade.php
@@ -10,10 +10,10 @@
             <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
                 <h1 class="text-3xl font-semibold text-gray-900 mb-6">Clases Disponibles</h1>
 
-                <!-- Mostrar el botón para crear clases solo si el usuario es entrenador o admin_entrenador -->
+                <!-- Mostrar el botón para crear clases solo si el usuario tiene el permiso adecuado -->
                 @canany(['crear_clases', 'admin_entrenador', 'entrenador'])
                     <div class="mb-6 text-right">
-                        <a href="{{ route('clases.create') }}" class="inline-block bg-green-500 text-white py-2 px-4 rounded-md hover:bg-green-600 focus:outline-none focus:ring-2 focus:ring-green-300 transition duration-200">
+                        <a href="{{ route('admin.entrenador.clases.create') }}" class="inline-block bg-green-500 text-white py-2 px-4 rounded-md hover:bg-green-600 focus:outline-none focus:ring-2 focus:ring-green-300 transition duration-200">
                             Crear Clase
                         </a>
                     </div>
