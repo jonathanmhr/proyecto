9178c4a78d4fca0af2a179b67b21b07c9e322d84
jonathan.michieli <jonathanmichelirosas@gmail.com>
2025-04-29 20:44:49 +0200

    se cambian vistas del usuario

--- resources/views/components/menu-gimnasio.blade.php
+++ resources/views/components/menu-gimnasio.blade.php
@@ -1,3 +1,27 @@
-<div>
-    <!-- Nothing in life is to be feared, it is only to be understood. Now is the time to understand more, so that we may fear less. - Marie Curie -->
+<div {{ $attributes->merge(['class' => 'space-y-1']) }}>
+    <x-responsive-nav-link href="{{ route('dashboard') }}" :active="request()->routeIs('dashboard')">
+        {{ __('Dashboard') }}
+    </x-responsive-nav-link>
+
+    @can('admin-access')
+        <x-responsive-nav-link href="{{ route('admin.users.index') }}" :active="request()->routeIs('admin.users.*')">
+            {{ __('Administración') }}
+        </x-responsive-nav-link>
+    @endcan
+
+    @can('entrenador-access')
+        <x-responsive-nav-link href="{{ route('entrenador.dashboard') }}" :active="request()->routeIs('entrenador.*')">
+            {{ __('Panel Entrenador') }}
+        </x-responsive-nav-link>
+    @endcan
+
+    <x-responsive-nav-link href="{{ route('entrenador.clases.index') }}" :active="request()->routeIs('entrenador.clases.*')">
+        {{ __('Clases') }}
+    </x-responsive-nav-link>
+
+    @canany(['admin_entrenador', 'entrenador-access'])
+        <x-responsive-nav-link href="{{ route('entrenador.clases.create') }}" :active="request()->routeIs('entrenador.clases.create')">
+            {{ __('Crear Clase') }}
+        </x-responsive-nav-link>
+    @endcanany
 </div>
