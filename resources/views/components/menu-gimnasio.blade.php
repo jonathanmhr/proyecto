f400f977ea8bb08c964b8f870a5e183ec3e66e2b
jonathan.michieli <jonathanmichelirosas@gmail.com>
2025-04-29 20:47:02 +0200

    se actualiza vista

--- resources/views/components/menu-gimnasio.blade.php
+++ resources/views/components/menu-gimnasio.blade.php
@@ -1,27 +1,27 @@
-<div {{ $attributes->merge(['class' => 'space-y-1']) }}>
-    <x-responsive-nav-link href="{{ route('dashboard') }}" :active="request()->routeIs('dashboard')">
+<div {{ $attributes->merge(['class' => 'flex flex-col sm:flex-row space-y-1 sm:space-y-0 sm:space-x-8']) }}>
+    <x-nav-link href="{{ route('dashboard') }}" :active="request()->routeIs('dashboard')">
         {{ __('Dashboard') }}
-    </x-responsive-nav-link>
+    </x-nav-link>
 
     @can('admin-access')
-        <x-responsive-nav-link href="{{ route('admin.users.index') }}" :active="request()->routeIs('admin.users.*')">
+        <x-nav-link href="{{ route('admin.users.index') }}" :active="request()->routeIs('admin.users.*')">
             {{ __('Administración') }}
-        </x-responsive-nav-link>
+        </x-nav-link>
     @endcan
 
     @can('entrenador-access')
-        <x-responsive-nav-link href="{{ route('entrenador.dashboard') }}" :active="request()->routeIs('entrenador.*')">
+        <x-nav-link href="{{ route('entrenador.dashboard') }}" :active="request()->routeIs('entrenador.*')">
             {{ __('Panel Entrenador') }}
-        </x-responsive-nav-link>
+        </x-nav-link>
     @endcan
 
-    <x-responsive-nav-link href="{{ route('entrenador.clases.index') }}" :active="request()->routeIs('entrenador.clases.*')">
+    <x-nav-link href="{{ route('entrenador.clases.index') }}" :active="request()->routeIs('entrenador.clases.*')">
         {{ __('Clases') }}
-    </x-responsive-nav-link>
+    </x-nav-link>
 
     @canany(['admin_entrenador', 'entrenador-access'])
-        <x-responsive-nav-link href="{{ route('entrenador.clases.create') }}" :active="request()->routeIs('entrenador.clases.create')">
+        <x-nav-link href="{{ route('entrenador.clases.create') }}" :active="request()->routeIs('entrenador.clases.create')">
             {{ __('Crear Clase') }}
-        </x-responsive-nav-link>
+        </x-nav-link>
     @endcanany
 </div>
