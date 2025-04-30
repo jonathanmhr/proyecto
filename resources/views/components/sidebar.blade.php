2672eb78cd0af17cb49dda7b68660fd9b479c680
jonathan.michieli <jonathanmichelirosas@gmail.com>
2025-04-30 17:01:11 +0200

    se quita el texto

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -35,7 +35,6 @@
             class="flex items-center gap-3 w-full text-gray-600 hover:bg-blue-100 hover:text-blue-600 px-3 py-2 rounded-lg text-sm transition-all group">
             {{ __('Profile') }}
             <i data-feather="settings" class="w-5 h-5"></i>
-            <span class="hidden group-hover:inline sm:inline">Ajustes</span>
         </a>
     </div>
 
