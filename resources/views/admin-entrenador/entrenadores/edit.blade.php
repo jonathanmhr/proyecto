894d4b1a7f3f4eb3f256d30d7f00c3e37d1f45cb
jonathan.michieli <jonathanmichelirosas@gmail.com>
2025-05-01 20:22:18 +0200

    se edita edita,blade.php para la gestion de entrenadores

--- resources/views/admin-entrenador/entrenadores/edit.blade.php
+++ resources/views/admin-entrenador/entrenadores/edit.blade.php
@@ -1,6 +1,7 @@
 <x-app-layout>
     <div class="container mx-auto mt-6">
-        <h2 class="text-2xl font-bold mb-4">Editar Entrenador: {{ $user->name }}</h2>
+        <h2 class="text-2xl font-bold mb-4">Editar Entrenador: {{ $entrenador->name }}</h2>
+
         
         <!-- Formulario de edición del entrenador -->
         <form action="{{ route('admin-entrenador.entrenadores.update', $entrenador->id) }}" method="POST">
@@ -11,14 +12,14 @@
                 <!-- Nombre -->
                 <div class="space-y-2">
                     <label for="name" class="block text-sm font-medium text-gray-700">Nombre</label>
-                    <input type="text" name="name" id="name" value="{{ old('name', $user->name) }}" 
+                    <input type="text" name="name" id="name" value="{{ old('name', $entrenador->name) }}"
                     class="w-full px-4 py-2 border rounded-lg focus:ring-2 focus:ring-blue-500" required>
                 </div>
                 
                 <!-- Correo -->
                 <div class="space-y-2">
                     <label for="email" class="block text-sm font-medium text-gray-700">Correo Electrónico</label>
-                    <input type="email" name="email" id="email" value="{{ old('email', $user->email) }}" 
+                    <input type="email" name="email" id="email" value="{{ old('email', $entrenador->email) }}"
                     class="w-full px-4 py-2 border rounded-lg focus:ring-2 focus:ring-blue-500" required>
                 </div>
 
