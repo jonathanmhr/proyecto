036e62190687233f4be8eee29866326a045b3d5b
jonathan.michieli <jonathanmichelirosas@gmail.com>
2025-05-03 00:20:48 +0200

    se edita el sidebar

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -71,8 +71,8 @@
 
         {{-- COMÚN A VARIOS ROLES --}}
         @canany(['cliente-access', 'entrenador-access', 'admin_entrenador'])
-        <x-sidebar-link icon="calendar" route="clases.index" label="Clases Disponibles" />
-            <x-sidebar-link icon="message-circle" route="clases.index" label="Comunidad" />
+        <x-sidebar-link icon="calendar" route="cliente.clases.index" label="Clases Disponibles" />
+            <x-sidebar-link icon="message-circle" route="cliente.clases.index" label="Comunidad" />
         @endcan
     </nav>
 
@@ -95,18 +95,4 @@
             </button>
         </form>
     </div>
-
-    <!--
-    <div class="mt-auto w-full px-2 sm:block hidden">
-        <form method="POST" action="{{ route('logout') }}" x-data>
-            @csrf
-            <button type="submit"
-                class="flex items-center gap-3 w-full text-gray-600 hover:bg-red-100 hover:text-red-600 px-3 py-2 rounded-lg text-sm transition-all"
-                :class="open ? 'justify-start' : 'justify-center'">
-                <i data-feather="log-out" class="w-5 h-5"></i>
-                <span x-show="open" x-cloak class="ml-2 transition-opacity duration-200">Cerrar sesión</span>
-            </button>
-        </form>
-    </div>
--->
 </aside>
