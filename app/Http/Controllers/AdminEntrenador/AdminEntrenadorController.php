6a300e90c4362c16a4deef726352f71d46c1ab00
jonathan.michieli <jonathanmichelirosas@gmail.com>
2025-05-03 00:29:56 +0200

    se edita controlador de admin entrenador

--- app/Http/Controllers/AdminEntrenador/AdminEntrenadorController.php
+++ app/Http/Controllers/AdminEntrenador/AdminEntrenadorController.php
@@ -10,6 +10,7 @@ use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
 use Silber\Bouncer\BouncerFacade as Bouncer;
+use Silber\Bouncer\Database\Role;
 use Auth;
 
 class AdminEntrenadorController extends Controller
