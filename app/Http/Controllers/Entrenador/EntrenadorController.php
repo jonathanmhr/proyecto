ef409856b328af04e8e2447ccba488752f72c54c
jonathan.michieli <jonathanmichelirosas@gmail.com>
2025-05-02 16:59:08 +0200

    editamos controlador de entrenador

--- app/Http/Controllers/Entrenador/EntrenadorController.php
+++ app/Http/Controllers/Entrenador/EntrenadorController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers\Entrenador;
 
 use App\Http\Controllers\Controller;
-use App\Models\Clase;
+use App\Models\ClaseGrupal;
 use App\Models\Entrenamiento;
 use App\Models\Suscripcion;
 
@@ -11,14 +11,14 @@ class EntrenadorController extends Controller
 {
     public function index()
     {
-        // Obtener todas las clases asociadas al entrenador logueado
-        $clases = Clase::where('entrenador_id', auth()->user()->id)->get();
+        // Obtener todas las clases grupales asociadas al entrenador logueado
+        $clases = ClaseGrupal::where('entrenador_id', auth()->user()->id)->get();
         
         // Obtener todos los entrenamientos del entrenador logueado
         $entrenamientos = Entrenamiento::where('entrenador_id', auth()->user()->id)->get();
         
         // Obtener todas las suscripciones activas del entrenador (a clases que tengan este entrenador)
-        $suscripciones = Suscripcion::whereHas('clase', function ($query) {
+        $suscripciones = Suscripcion::whereHas('claseGrupal', function ($query) {
             $query->where('entrenador_id', auth()->user()->id);
         })->get();
 
