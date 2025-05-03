8ee2353546fe3ce60063debacb308eabdbb1ee75
jonathan.michieli <jonathanmichelirosas@gmail.com>
2025-05-03 23:34:13 +0200

    corrige la ruta de la vista de edición de clase en el método edit del controlador del entrenador

--- app/Http/Controllers/Entrenador/EntrenadorController.php
+++ app/Http/Controllers/Entrenador/EntrenadorController.php
@@ -101,11 +101,10 @@ class EntrenadorController extends Controller
     // Método para editar los detalles de una clase
     public function edit($id)
     {
-        dd($id); // Para ver si el ID llega correctamente
         $clase = ClaseGrupal::findOrFail($id);
-        return view('entrenador.clase.edit', compact('clase'));
+        return view('entrenador.clases.edit', compact('clase'));
     }
-    
+
     // Método para actualizar los detalles de la clase, pero debe marcarse como pendiente para aprobación
     public function updateClase(Request $request, $id)
     {
