08525b76d979aa666adc239425f2a21392bea694
jonathan.michieli <jonathanmichelirosas@gmail.com>
2025-05-05 13:15:56 +0200

    refactor: mejora la legibilidad y estructura de las relaciones en el modelo User

--- app/Models/User.php
+++ app/Models/User.php
@@ -66,30 +66,43 @@ class User extends Authenticatable implements MustVerifyEmail
             'password' => 'hashed',
         ];
     }
-    // QUERYS
+    
+    // RELACIONES
     public function clases()
     {
         return $this->belongsToMany(ClaseGrupal::class, 'suscripciones', 'id_usuario', 'id_clase')
-            ->wherePivot('estado', Suscripcion::ESTADO_ACTIVO)  // Filtrar solo las suscripciones activas
-            ->wherePivot('fecha_fin', '>', now());  // Filtrar solo las suscripciones con fecha de fin futura
+            ->wherePivot('estado', Suscripcion::ESTADO_ACTIVO)  // Filtrar solo suscripciones activas
+            ->wherePivot('fecha_fin', '>', now());  // Filtrar suscripciones con fecha de fin futura
     }
-
+    
+    // Relación: Clases grupales asociadas al entrenador
     public function clasesGrupales()
     {
         return $this->hasMany(ClaseGrupal::class, 'entrenador_id');
     }
+    
+    // Relación: Usuarios inscritos en clases grupales
     public function usuarios()
     {
         return $this->belongsToMany(User::class, 'suscripciones', 'id_clase', 'id_usuario')
-            ->withPivot('estado', 'fecha_inicio', 'fecha_fin');  // Para manejar los datos adicionales de la suscripción
+            ->withPivot('estado', 'fecha_inicio', 'fecha_fin');  // Datos adicionales de la suscripción
     }
+    
+    // Relación: Reservas de clases realizadas por el usuario
     public function reservasDeClases()
     {
         return $this->hasMany(ReservaDeClase::class, 'id_usuario');
     }
-
+    
+    // Relación: Perfil de usuario asociado
     public function perfilUsuario()
-{
-    return $this->hasOne(PerfilUsuario::class, 'id_usuario', 'id');
-}
-}
+    {
+        return $this->hasOne(PerfilUsuario::class, 'id_usuario', 'id');
+    }
+    
+    // Relación: Suscripciones asociadas a la clase grupal
+    public function suscripciones()
+    {
+        return $this->hasMany(Suscripcion::class, 'id_clase');
+    }
+    
