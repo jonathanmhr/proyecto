e1b1f0df4648b629768c3b9bac4c65afe622e7f8
jonathan.michieli <jonathanmichelirosas@gmail.com>
2025-05-07 20:18:13 +0200

    se edita config deel vite

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ export default defineConfig({
     server: {
         host: true, // Esto permite conexiones externas
         hmr: {
-            host: '192.168.1.25',
+            host: '192.168.1.27',
         }
     },
     plugins: [
