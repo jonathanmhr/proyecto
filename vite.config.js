642ed368f0ad212d538feadd2a0a4f6fa6a47d9f
jonathan.michieli <jonathanmichelirosas@gmail.com>
2025-05-20 19:49:14 +0200

    Revert "agregamos cambio ip"
    This reverts commit 4de9f572dc67967f35f96771a3b6ae49283ecbe5.

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ export default defineConfig({
     server: {
         host: true, // Esto permite conexiones externas
         hmr: {
-            host: '192.168.1.26',
+            host: '192.168.1.27',
         }
     },
     plugins: [
