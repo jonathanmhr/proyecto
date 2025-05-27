f8f6d66669f93cee8b82af2de31b9c75bbe7ac42
jonathan.michieli <jonathanmichelirosas@gmail.com>
2025-05-27 23:36:34 +0200

    refactor: elimina la importación y configuración de Alpine.js en app.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,11 +1,3 @@
 import './bootstrap';
 import '../css/app.css';
-
-// Alpine.js import y configuración
-import Alpine from 'alpinejs';
-
-window.Alpine = Alpine;
-Alpine.start();
-
-// Importación de scripts adicionales
 import './scripts/busqueda.js';
