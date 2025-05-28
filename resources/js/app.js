19e3c075fbd7f9aa027732e714540397ee60284b
jonathan.michieli <jonathanmichelirosas@gmail.com>
2025-05-28 19:27:03 +0200

    fix: mejora la importación de ApexCharts y organiza el código en app.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,14 +1,16 @@
 import './bootstrap';
 import '../css/app.css';
 import './scripts/busqueda.js';
+
 import Alpine from 'alpinejs';
+import ApexCharts from 'apexcharts';
 
 window.Alpine = Alpine;
+window.ApexCharts = ApexCharts;
 
 document.addEventListener('DOMContentLoaded', () => {
     Alpine.start();
 
-    // feather está disponible globalmente por el CDN
     if (window.feather) {
         window.feather.replace();
     }
