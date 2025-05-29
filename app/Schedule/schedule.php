75a26436728e39c57bb60ec9762dacf246a07699
jonathan.michieli <jonathanmichelirosas@gmail.com>
2025-05-29 22:51:21 +0200

    Borrando todo el contenido de master

--- app/Schedule/schedule.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-use Illuminate\Foundation\Scheduling\Schedule;
-use App\Console\Commands\DesactivarSuscripcionesVencidas;
-
-return function (Schedule $schedule) {
-    $schedule->command(DesactivarSuscripcionesVencidas::class)->everyMinute();
-};
--- resources/views/components/label.blade.php
+++
@@ -1,5 +0,0 @@
-@props(['value'])
-
-<label {{ $attributes->merge(['class' => 'block font-medium text-sm text-gray-100']) }}>
-    {{ $value ?? $slot }}
-</label>
