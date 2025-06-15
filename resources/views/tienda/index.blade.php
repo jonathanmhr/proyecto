b46a8265051266a32670044a55c2556518157d9f
Adrian-Alba-01 <adrianalbasevilla01@gmail.com>
2025-06-15 21:28:27 +0200

    Arreglo imagen svg

--- resources/views/tienda/index.blade.php
+++ resources/views/tienda/index.blade.php
@@ -86,7 +86,6 @@
     <div class="container mx-auto px-4 py-8">
         <h1 class="text-3xl font-bold text-red-500 mb-6 text-center md:text-left">Nuestros Productos</h1>
 
-        <!-- === INICIO: FORMULARIO DE FILTROS INTEGRADO CON EL TEMA OSCURO === -->
         <form method="GET" action="{{ route('tienda.index') }}" class="bg-gray-800 p-6 rounded-2xl shadow-lg border border-gray-700 mb-8">
             <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-5 gap-x-6 gap-y-8 items-end">
 
@@ -104,7 +103,7 @@
                             name="search"
                             id="search"
                             value="{{ $searchTerm ?? '' }}"
-                            class="w-full pl-11 pr-4 py-2.5 bg-gray-700 text-gray-200 border-gray-600 rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-red-500 focus:border-red-500 transition placeholder-gray-500"
+                            class="w-full pl-12 pr-4 py-2.5 bg-gray-700 text-gray-200 border-gray-600 rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-red-500 focus:border-red-500 transition placeholder-gray-500"
                         >
                     </div>
                 </div>
