7e20f7de7e2994259764ceddf899074dc5509d00
Adrian-Alba-01 <adrianalbasevilla01@gmail.com>
2025-06-15 21:33:37 +0200

    Cambio en placeholder

--- resources/views/tienda/index.blade.php
+++ resources/views/tienda/index.blade.php
@@ -94,7 +94,7 @@
                 <div class="lg:col-span-2">
                     <label for="search" class="block text-sm font-semibold text-gray-400 mb-2">Buscar por producto</label>
                     <div class="relative">
-                        <div class="absolute mr-1 inset-y-0 left-0 flex items-center pl-3.5 pointer-events-none">
+                        <div class="absolute pr-1 inset-y-0 left-0 flex items-center pl-3.5 pointer-events-none">
                             <svg class="w-5 h-5 text-gray-500" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor">
                                 <path stroke-linecap="round" stroke-linejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
                             </svg>
